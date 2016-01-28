3ec0f350339851be505941472ca898ce784333db
nmerci <nmerci@mail.com>
2016-01-28 23:15:35 +0200

    aggregated data in JSON format are added. Closes #7.

--- R/data_preprocessing.R
+++ R/data_preprocessing.R
@@ -5,6 +5,20 @@ if(!require("data.table"))
     library("data.table")
 }
 
+#load jsonlite package
+if(!require("jsonlite"))
+{
+  install.packages("jsonlite")
+  library("jsonlite")
+}
+
+#load lubridate package
+if(!require("lubridate"))
+{
+  install.packages("lubridate")
+  library("lubridate")
+}
+
 #read data
 df <- fread(input="data/wa_data.csv")
 
@@ -15,23 +29,19 @@ df[, is_checkout_page:=as.numeric(as.character(df$is_checkout_page))]
 
 #convert time in POSIX format
 df[, time:=substr(df$time, 1, 19)]
-df[, time:=as.numeric(format(strptime(df$time, format="%F %T"), format="%s"))]
+df[, time:=as.numeric(format(fast_strptime(df$time, format="%Y-%m-%d %H:%M:%S"), format="%s"))]
 setorder(df, time)
 
 #aggregate data by session
-df <- merge(x=df[, .(time=list(time),
-                     page_id=list(page_id),
-                     source_id=list(source_id),
-                     is_checkout_page=list(is_checkout_page)), by=session_id],
-            y=unique(df[, .(session_id, visitor_id, user_agent, cc, cloc)], by="session_id"),
-            by="session_id")
+aggregated_data <- df[, .(time=list(time),
+                          page_id=list(page_id),
+                          source_id=list(source_id),
+                          is_checkout_page=list(is_checkout_page)), by=session_id]
 
-#convert to string for writing in file
-#!this should be rewriten in a smart way
-df[, time:=as.character(time)]
-df[, page_id:=as.character(page_id)]
-df[, source_id:=as.character(source_id)]
-df[, is_checkout_page:=as.character(is_checkout_page)]
+session_data <- unique(df[, .(session_id, visitor_id, user_agent, cc, cloc)], by="session_id")
 
-#save processed data to file
-write.csv(df, file="data/preprocessed_data1.csv", row.names=F)
+#save aggregated_data in JSON format
+write(toJSON(aggregated_data), file="data/aggregated_data.json")
+
+#save session_data in CSV format
+write.csv(session_data, file="data/session_data.csv", row.names=F)
